1eac1ed84ce166738acf255ed964cbe0d7a8a2f8
J. Kristoffer Legind <klegind@gmail.com>
2022-05-19 14:20:38 +0200

    Added REPLACE(...taxonFullName, '*', '')
    Got us down from 2110 to 2107 records

--- Taxon Spine/SQL/TaxonSpine_Tracheophyta.sql
+++ Taxon Spine/SQL/TaxonSpine_Tracheophyta.sql
@@ -1,22 +1,33 @@
 SELECT 				
         tdivis.TaxonTreeDefID AS taxonTree,
-        tdivis.TaxonID Division, 
+        --tdivis.TaxonID Division, 
+        tdivis.name,
         tclass.TaxonID Class,
+        tclass.name,
         torder.TaxonID `Order`, 
+        torder.name,
         tfamil.TaxonID Family,
-		  tgenus.TaxonID Genus,
+		  tfamil.name, 
+        tgenus.TaxonID Genus,
+        tgenus.name,
+        -- tgenus.Name Genusname,
         tspeci.TaxonID Species,
-		  tsubsp.TaxonID Subspecies,
-		  tvarie.TaxonID Variety, 
+		  tspeci.name, 
+        -- tspeci.Name Speciesname,
+        tsubsp.TaxonID Subspecies,
+		  tsubsp.name ,
+        -- tsubsp.Name SubspeciesName,
+        tvarie.TaxonID Variety, 
+         tvarie.Name VarietyName,
         tforma.TaxonID Forma, 
-        COALESCE(tforma.TaxonID, tvarie.TaxonID, tsubsp.TaxonID, tspeci.TaxonID, tgenus.TaxonID, tfamil.TaxonID, torder.TaxonID) taxonID,
-		  COALESCE(IF(tforma.FullName IS NOT NULL,CONCAT(tgenus.name, ' ', tspeci.Name, ' ', tsubsp.Name, ' ', ' f. ', tforma.Name),NULL), 
+        -- tforma.Name FormaName,
+        COALESCE(tforma.TaxonID, tvarie.TaxonID, tsubsp.TaxonID, tspeci.TaxonID, tgenus.TaxonID, tfamil.TaxonID, torder.TaxonID) lowest_taxonID,
+		  REPLACE(COALESCE(IF(tforma.FullName IS NOT NULL,CONCAT(tgenus.name, ' ', tspeci.Name, ' ', tsubsp.Name, ' ', ' f. ', tforma.Name),NULL), 
 		           IF(tvarie.FullName IS NOT NULL,CONCAT(tgenus.name, ' ', tspeci.Name, ' var. ', tvarie.Name),NULL),
 					  IF(tsubsp.FullName IS NOT NULL,CONCAT(tgenus.name, ' ', tspeci.Name, ' ', tsubsp.Name),NULL), 
-					  tspeci.FullName, tgenus.FullName, tfamil.FullName, torder.FullName, tclass.FullName, tdivis.FullName) TaxonFullName,	
-		  COALESCE(tforma.Name, tvarie.Name, tsubsp.Name, tspeci.Name, tgenus.Name, tfamil.Name) TaxonName, 
-		  COALESCE(tforma.rankID, tvarie.rankID, tsubsp.rankID, tspeci.rankID, tgenus.rankID, tfamil.RankID) rankID, 
-		  tclass.TaxonID ClassID
+					  tspeci.FullName, tgenus.FullName, tfamil.FullName, torder.FullName, tclass.FullName, tdivis.FullName), '*', '') TaxonFullName,
+			#COALESCE(defitem.RankID, tforma.rankID, tvarie.rankID, tsubsp.rankID, tspeci.rankID, tgenus.rankID, tfamil.rankID) rankID 
+			 COALESCE(tforma.rankID, tvarie.rankID, tsubsp.rankID, tspeci.rankID, tgenus.rankID, tfamil.RankID) rankID
     FROM taxon tdivis 
         LEFT JOIN taxon tclass ON tclass.ParentID = tdivis.TaxonID AND tclass.RankID = 60  AND tclass.IsAccepted = TRUE 
         LEFT JOIN taxon torder ON torder.ParentID = tclass.TaxonID AND torder.RankID = 100 AND torder.IsAccepted = TRUE  
@@ -28,7 +39,6 @@ SELECT
         LEFT JOIN taxon tforma ON tforma.ParentID = tspeci.TaxonID AND tforma.RankID = 260 AND tforma.IsAccepted = TRUE
 		  LEFT JOIN taxontreedefitem defitem ON tdivis.TaxonTreeDefItemID = defitem.TaxonTreeDefItemID
 		  
-    WHERE tdivis.ParentID = 317960
+    WHERE tdivis.ParentID = 317960 -- Plantae kingdom  
       AND tdivis.TaxonTreeDefID = 13
-      GROUP BY TaxonFullName 
-      
+		GROUP BY TaxonFullName ; -- Tree belonging to discipline: Botany
