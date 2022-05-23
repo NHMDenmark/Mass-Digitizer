f388490739c5cb45408f04ab7b6b70092796516a
J. Kristoffer Legind <klegind@gmail.com>
2022-05-23 12:27:00 +0200

    added ORDER BY to the string_agg

--- Taxon Spine/SQL/consolidate_duplicate_fullTaxonNames.sql
+++ Taxon Spine/SQL/consolidate_duplicate_fullTaxonNames.sql
@@ -1,6 +1,6 @@
 #Let us say that we have created a table based on TaxonSpine_Tracheophyta.sql, and we have imported this into a PostgresQL DB (for reasons to do with 'similarity', 'Levenschtein distance' and other string comparisons that MySQL does not have)
 #We then do a simple COUNT GROUP BY taxonfullname. We make sure to keep old lowest taxon IDs to be able to reconnect to collection objects in Specify later. 
 
-SELECT t1.taxonfullname as t1full , string_agg(t1.lowest_taxonid::text, ',') as low_taxonIDs, count(*) as cnt
+SELECT t1.taxonfullname as t1full , string_agg(t1.lowest_taxonid::text, ',' order by t1.lowest_taxonid) as low_taxonIDs, count(*) as cnt
 FROM specify_taxonomy  t1 
 group by 1 having count(*) > 1;
