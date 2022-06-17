95673b77978c43637f9a21235b1af927522c0085
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2022-06-17 13:56:17 +0200

    Structured code into functions

--- MADD_app/SpecifyFetch.py
+++ MADD_app/SpecifyFetch.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-  
 import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -8,49 +7,77 @@ from getpass import getpass
 baseURL = "https://specify-test.science.ku.dk/"
 loginURL = baseURL + "context/login/"
 
+# global variables 
+# 
 # Create a session for storing cookies 
-sess = requests.Session() 
+spSession = requests.Session() 
+ 
+
+def getCSRFToken():
+  print('get CSRF token')
+  response = spSession.get(loginURL, verify=False)
+  csrftoken = response.cookies.get('csrftoken')
+  print('response: ' + str(response.status_code) + " " + response.reason)
+  print('CSRF Token: ', csrftoken)
+  print('----------')
+  return csrftoken
+
+def login(username, passwd, csrftoken):
+  print('log in using CSRF token & username/password')
+  headers = {'content-type': 'application/json', 'X-CSRFToken': csrftoken, 'Referer': baseURL}
+  response = spSession.put(baseURL + "context/login/", json={"username": username, "password": passwd, "collection": 851970}, headers=headers, verify=False)
+  csrftoken = response.cookies.get('csrftoken') # Keep and use new CSRF token after login
+  print('response: ' + str(response.status_code) + " " + response.reason)
+  print('New CSRF Token: ', csrftoken)
+  print('----------')
+  return csrftoken
+
+def verifySession(csrftoken):
+  #verify session
+  print('verify session')
+  headers = {'content-type': 'application/json', 'X-CSRFToken': csrftoken, 'Referer': baseURL}
+  response = spSession.get(baseURL + "context/user.json", headers=headers)
+  print('response: ' + str(response.status_code) + " " + response.reason)
+  if response.status_code > 299:
+    # print(response.text)
+    print('something went wrong...')
+  else:
+    print('user id: ' + str(response.json()['id']))
+  print('----------')
+
+def queryCollObject(collectionObjectId, csrftoken):
+  #query object 
+  print('query object')
+  headers = {'content-type': 'application/json', 'X-CSRFToken': csrftoken, 'Referer': baseURL}
+  response = spSession.get(baseURL + "api/specify/collectionobject/" + str(collectionObjectId)  + "/", headers=headers)
+  print('response: ' + str(response.status_code) + " " + response.reason)
+  print('catalog number: ' + response.json()['catalognumber'])
+  print('----------')
+
+def logout(csrftoken):
+  headers = {'content-type': 'applicatiob/json', 'X-CSRFToken': csrftoken, 'Referer': baseURL}
+  response = spSession.put(baseURL + "context/login/", data="{\"username\": null, \"password\": null, \"collection\": 688130}", headers=headers)
+  print(str(response.status_code) + " " + response.reason)
+  print('----------')
+
 
 #Connect to Specify7 API
 # First step is to get a CSRF token 
-print('get CSRF token')
-response = sess.get(loginURL, verify=False)
-csrftoken = response.cookies.get('csrftoken')
-print('response: ' + str(response.status_code) + " " + response.reason)
-print('CSRF Token: ', csrftoken)
-
+csrftoken = getCSRFToken()
 # Ask for username and password from user
 print('get username/password from input')
 username = input()
 passwd = getpass()
 print('username: "%", password: "%"', username, passwd)
-
 # Next step is to use csrf token to log in 
-print('log in using CSRF token & username/password')
-headers = {'content-type': 'application/json', 'X-CSRFToken': csrftoken, 'Referer': baseURL}
-response = sess.put(baseURL + "context/login/", json={"username": username, "password": passwd, "collection": 851970}, headers=headers, verify=False)
-cookies = response.cookies
-csrftoken = response.cookies.get('csrftoken') # Keep and use new CSRF token after login
-print('response: ' + str(response.status_code) + " " + response.reason)
-print('New CSRF Token: ', csrftoken)
+csrftoken = login(username, passwd, csrftoken)
+#verify session to check all is OK 
+verifySession(csrftoken)
+# query some random collection object 
+queryCollObject(501269, csrftoken)
+#log out
+logout(csrftoken)
 
-#verify session
-print('verify session')
-headers = {'content-type': 'application/json', 'X-CSRFToken': csrftoken, 'Referer': baseURL}
-response = sess.get(baseURL + "context/user.json", headers=headers)
-print('response: ' + str(response.status_code) + " " + response.reason)
-if response.status_code > 299:
-  # print(response.text)
-  print('something went wrong...')
-else:
-  print('user id: ' + str(response.json()['id']))
-
-#query object 
-print('query object')
-response = sess.get(baseURL + "api/specify/collectionobject/501269/", headers=headers)
-print('response: ' + str(response.status_code) + " " + response.reason)
-print('catalog number: ' + response.json()['catalognumber'])
-print()
 
 """
   Created on Tuesday June 14, 2022
