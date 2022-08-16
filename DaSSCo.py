706eee11b9d1b82c857cc585b67b4fa985d162ed
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2022-08-16 14:16:01 +0200

    Restructured project to enable compilation
    When residing in the "src" folder, the base code was unreachable during runtime of the executable.

--- DaSSCo.py
+++ DaSSCo.py
@@ -1,12 +1,12 @@
 import sys
 import easygui
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent.joinpath('src')))
+#sys.path.append(str(Path(__file__).parent.joinpath('src')))
 
-easygui.msgbox(str(Path(__file__).parent.joinpath('src')), "DEBUG")
+#easygui.msgbox(str(Path(__file__).parent.joinpath('src')), "DEBUG")
 
 import MassDigitizer.__main__ as main
 
 if __name__ == '__main__':
-    print(str(Path(__file__).parent.joinpath('src')))
-    main()
+    #main.main()
+    pass
