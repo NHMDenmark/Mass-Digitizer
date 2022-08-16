9dec8dd7dd0790a6a63855dcd22d1d0f03e741c7
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2022-08-16 14:16:01 +0200

    Restructured project to enable compilation
    When residing in the "src" folder, the base code was unreachable during runtime of the executable.

---
+++ MassDigitizer/__main__.py
@@ -0,0 +1,16 @@
+import sys
+import MassDigitizer.home_screen as home
+
+def main() -> None:
+    print("* Mass Digitizer for DaSSCo *")
+    home.start()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
