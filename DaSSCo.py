90ed3644befebddc20e395297657094f620af71a
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2022-08-19 10:43:35 +0200

    Fixed app start #66

--- DaSSCo.py
+++ DaSSCo.py
@@ -1,12 +1,9 @@
 import sys
-import easygui
 from pathlib import Path
-#sys.path.append(str(Path(__file__).parent.joinpath('src')))
-
-#easygui.msgbox(str(Path(__file__).parent.joinpath('src')), "DEBUG")
+sys.path.append(str(Path(__file__).parent.joinpath('MassDigitizer')))
 
 import MassDigitizer.__main__ as main
 
 if __name__ == '__main__':
-    #main.main()
+    main.main()
     pass
