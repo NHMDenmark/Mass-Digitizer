f58769e56d10d45e0694839a2c617d2baba065ce
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2022-08-19 10:44:59 +0200

    Attempt at auto-merging taxa #74

--- MassDigitizer/util.py
+++ MassDigitizer/util.py
@@ -1,5 +1,6 @@
 from hashlib import new
 from os import system, name
+import requests
 
 def clear():
    # Clear CLI screen 
@@ -30,3 +31,21 @@ def convert_dbrow_list(list):
       new_list.append(item['name'])
 
    return new_list
+
+def pretty_print_POST(req):
+    """
+    At this point it is completely built and ready
+    to be fired; it is "prepared".
+
+    However pay attention at the formatting used in 
+    this function because it is programmed to be pretty 
+    printed and may differ from the actual request.
+    """
+    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        req.body,
+    ))
+    print('------------END------------')
+
