fc7e26694f9ebcd33d7980ca81e8663a3593e199
J. Kristoffer Legind <klegind@gmail.com>
2022-08-22 12:31:11 +0200

    latest bare bones UI interface

--- MassDigitizer/specimen_data_entry-backup.py
+++ MassDigitizer/specimen_data_entry-backup.py
@@ -17,9 +17,14 @@ software distributed under the License is distributed on an "AS IS" BASIS, WITHO
 either express or implied. See the License for the specific language governing permissions and limitations under the License.
 """
 import PySimpleGUI as sg
-# import import_csv_memory
-import util
+import os
+import sys
 
+import pathlib
+sys.path.append(str(pathlib.Path(__file__).parent.parent.joinpath('MassDigitizer')))
+
+currentpath = os.path.join(pathlib.Path(__file__).parent, '')
+print(currentpath)
 
 sg.theme('Material2')
 blueArea = '#99ccff'
@@ -39,66 +44,63 @@ barcode = [58697014]
 collections = ['Botany', 'Entomology', 'Ichthyology']
 workstations = ['Commodore_64', 'VIC_20', 'HAL2000', 'Cray']
 
-defaultSize = (21,1)
+defaultSize = (20,1)
 #defaultSize is used to space all data entry elements so that they line up.
-font = ('Bahnschrift', 13)
+font = ('Bahnschrift', 12)
 element_size = (30,1)
-blue_size = (28,1)
+#size for making all fields uniform
 
 #Input elements (below) are stored in variables with brackets to make it easier to include and position in the frames
 storage = [
                     sg.Text("Storage location:", size=defaultSize, background_color=greenArea, font=font),
-                    sg.Combo(institutions, key='-STORAGE-', size=element_size, text_color='black', background_color='white', font=('Arial', 12), enable_events=True),
+                    sg.Combo(institutions, key='-STORAGE-', text_color='black', background_color='white'),
                 ]
 
 preparation = [
                     sg.Text("Preparation type:", size=defaultSize, background_color=greenArea, font=font),
-                    sg.Combo(prepType, key='-PREP-', size=element_size, text_color='black', background_color='white', enable_events=True),
+                    sg.Combo(prepType, key='-PREP-', size=element_size ,text_color='black', background_color='white'),
                     ]
 
 taxonomy = [
                     sg.Text("Taxonomic group:", size=defaultSize, background_color=greenArea, font=font),
-                    sg.Combo(taxonomicGroups, key='-TAXON-', size=element_size, text_color='black', background_color='white', enable_events=True),
+                    sg.Combo(taxonomicGroups, size=element_size, key='-TAXON-',text_color='black', background_color='white'),
                     ]
 
 type_status = [
                     sg.Text('Type status:', size=defaultSize, background_color=greenArea, font=font),
-                    sg.Combo(typeStatus, key='-TYPE-', size=element_size, text_color='black', background_color='white', font=font, enable_events=True),
+                    sg.Combo(typeStatus, key='-TYPE-', size=element_size, text_color='black', background_color='white', font=font),
                  ]
 
-notes = [sg.Text('Notes', size=defaultSize, background_color=greenArea, font=font),
-         sg.Multiline(size=(29,5), background_color='white', text_color='black', key='-NOTES-', enable_events=False)]
-# note_box = [sg.Multiline(size=(24,5), background_color='white', text_color='black', key='-NOTES-')]
+notes_title = [sg.Text('Notes', size=defaultSize, background_color=greenArea, font=font)]
+
+
+notes_box = [ sg.Multiline(size=(24,5), background_color='white', text_color='black', key='-NOTES-'),]
 
 layout_greenarea = [
-    storage, preparation, taxonomy, type_status, notes, [sg.Checkbox('Multispecimen sheet', background_color=greenArea, font=(11))],
+    storage, preparation, taxonomy, type_status, notes_title, notes_box, [sg.Checkbox('Multispecimen sheet', background_color=greenArea)],
     ]
 #Above is the so-called 'green area'
 
 broadGeo = [
                     sg.Text('Broad geographic region:', size=defaultSize ,background_color=blueArea, text_color='black', font=font),
-                    sg.Combo(geoRegionsCopenhagen, size=blue_size, key='-GEOREGION-', text_color='black', background_color='white', enable_events=True),
+                    sg.Combo(geoRegionsCopenhagen, size=(28,1), key='-GEOREGION-', text_color='black', background_color='white'),
                  ]
 
 taxonomicName = [
                     sg.Text('Taxonomic name:', size=defaultSize, background_color=blueArea, text_color='black', font=font),
-                    sg.Combo(taxonomicNames, key='-TAXNAMES-', size=blue_size, text_color='black', background_color='white', enable_events=True),
+                    sg.InputText('', size=element_size, key='-TAXNAMES-', text_color='black', background_color='white'),
                  ]
 
 barcode = [
                     sg.Text('Barcode:', size=defaultSize, background_color=blueArea, text_color='black', font=font),
-                    sg.InputText('', key='-BARCODE-', size=blue_size, text_color='black', background_color='white', enable_events=True),
+                    sg.StatusBar('', size=(15,1), key='-BARCODE-', text_color='black', background_color='white'),
                  ]
 
-# button_frame = [sg.Frame(layout=[[sg.Button('SAVE', key='-SAVE-', button_color='seagreen'), sg.StatusBar('', relief=None, size=(30,1), background_color=blueArea),
-#                                   sg.Button('Go back', key='-GOBACK', button_color='firebrick'),
-#         sg.Sizer(280, 10)]] , title='', relief=None, background_color=blueArea, border_width=0)]
 
 
 layout_bluearea = [
     broadGeo, taxonomicName, barcode,
-    # button_frame,
-    [sg.StatusBar('', relief=None, size=(32,1), background_color=blueArea), sg.Button('SAVE', key="-SAVE-", button_color='seagreen'), sg.StatusBar('', relief=None, size=(20,1), background_color=blueArea), sg.Button('Go Back', key="-GOBACK-", button_color='firebrick', pad=(120,0))]
+    [sg.Button('SAVE', key="-SAVE-", button_color='seagreen'), sg.Push(background_color=blueArea),sg.Text('                                      ', background_color=blueArea), sg.Push(background_color=blueArea), sg.Button('Go Back', key="-GOBACK-", button_color='firebrick', pad=(120,0))]
 ]
 
 loggedIn = [sg.Text('Logged in as:', size=defaultSize, background_color=greyArea, font=font), sg.InputText(size=(24,1), background_color='white', text_color='black', key='-LOGGED-'),
@@ -116,8 +118,7 @@ collections =  [sg.Text('Collection name:', size=defaultSize, background_color=g
 work_station =  [sg.Text('Workstation:', size=defaultSize, background_color=greyArea, font=font), sg.Combo(workstations, key="-WORKSTATION-", size=element_size, text_color='black', background_color='white'),
          ]
 
-settings_ = [sg.Text('Settings ', size=defaultSize, justification='center', background_color=greyArea, font=14), sg.Button('', image_filename=r'options_gear.png',
-                                                                                          button_color=greyArea, key='-SETTING-', border_width=0)
+settings_ = [sg.Text('Settings ', size=defaultSize, justification='center', background_color=greyArea, font=('Bahnschrift', 10)), sg.Button('', image_filename='%soptions_gear.png'%currentpath, button_color=greyArea, key='-SETTING-', border_width=0)
          ]
 
 layout_greyarea = [loggedIn, dateTime, [sg.Text("_______________" * 5, background_color=greyArea)], institution_,
@@ -130,33 +131,22 @@ layout = [
     [sg.Frame('',   [[sg.Column(layout_bluearea, background_color=blueArea)]], expand_x=True, expand_y=True, background_color=blueArea, title_location=sg.TITLE_LOCATION_TOP)],
 ]
 
-window = sg.Window("Simple Annotated Digitization Desk  (SADD)", layout, margins=(2, 2), size=(900,500), resizable=True, finalize=True )
-#The three lines below are there to ensure that the cursor in the input text fields is visible. It is invisible against a white background.
-window['-NOTES-'].Widget.config(insertbackground='black', highlightcolor='firebrick', highlightthickness=2)
-window['-LOGGED-'].Widget.config(insertbackground='black', highlightcolor='firebrick', highlightthickness=2)
-window['-DATETIME-'].Widget.config(insertbackground='black', highlightcolor='firebrick', highlightthickness=2)
+def init(collection_id):
+    window = sg.Window("Simple Annotated Digitization Desk  (SADD)", layout, margins=(2, 2), size=(900,500), resizable=True, finalize=True, )
+    #The three lines below are there to ensure that the cursor in the input text fields is visible. It is invisible against a white background.
+    window['-NOTES-'].Widget.config(insertbackground='black', highlightcolor='firebrick', highlightthickness=2)
+    window['-LOGGED-'].Widget.config(insertbackground='black', highlightcolor='firebrick', highlightthickness=2)
+    window['-DATETIME-'].Widget.config(insertbackground='black', highlightcolor='firebrick', highlightthickness=2)
 
-while True:
+    while True:
 
-    event, values = window.read()
-    print(event, '##', values)
-    #SWITCH CONSTRUCT
-    if event == '-STORAGE-':
-        print('event:', event)
-        print('In storage domain')
-    if event == '-PREP-':
-        print('In preparation type')
-    if event == '-TAXON-':
-        print('IN taxonomy section')
-    if event == '-TYPE-':
-        print('IN type status section')
-    if event == '-NOTES-':
-        print('IN notes section')
-    if event == sg.WINDOW_CLOSED:
-        break
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED:
+            break
 
-window.close()
+    window.close()
 
+init(1)
 
 """ TO DO:
     Restrict the characters allowed in an input element to digits and . or -
