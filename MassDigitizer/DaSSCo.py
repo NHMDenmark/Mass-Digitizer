2278045cb0995f6b4cefc7ed519d800e92df24ad
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2022-09-19 18:10:03 +0200

    renamed __main__.py to DaSSCo.py

---
+++ MassDigitizer/DaSSCo.py
@@ -0,0 +1,16 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.joinpath('')))
+
+import home_screen as home
+import data_access as db
+import specify_interface as sp
+
+def main() -> None:
+    print("* Mass Digitizer for DaSSCo *")
+    home.init()
+
+    # TODO start background processes? 
+
+if __name__ == "__main__":
+    main()
