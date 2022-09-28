e3e627e8f82f0c60597a1f087abc971651c5e94d
J. Kristoffer Legind <klegind@gmail.com>
2022-09-28 12:42:30 +0200

    added a bit of documentation

--- MassDigitizer/storage_autosuggest_research.py
+++ MassDigitizer/storage_autosuggest_research.py
@@ -5,14 +5,14 @@ from timeit import default_timer as timer
 from datetime import timedelta
 
 
-start = timer()
+start = timer() #performance measure start
 sql = "SELECT name, fullname FROM storage"
 rows = db.executeSqlStatement(sql)
 storageDict = {}
 for j in rows:
     storageDict[j['name']] = j['fullname']
 
-end = timer()
+end = timer() #end of measuring the timedelta of populating the storageDict.
 print('Time performance for populating dictionary storageDict= ', timedelta(seconds=end-start))
 
 print(len(storageDict))
@@ -21,6 +21,7 @@ def storage_autosuggest(partialName, storage_dictionary):
 
     print('IN storage_auto!! partial=', partialName)
     keys = fnmatch.filter(storage_dictionary, "Box 38*")
+    # fnmatch is a unix style pattern matching module
     print('length return dict: ', len(keys))
 
     return keys
