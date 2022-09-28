b36cd0dbf658ca13f1a9b9043387a40c7c92963b
J. Kristoffer Legind <klegind@gmail.com>
2022-09-28 12:26:36 +0200

    Create storage_autosuggest_research.py
    A proof of concept for storage auto-suggest. Contains speed performance measurement.

---
+++ MassDigitizer/storage_autosuggest_research.py
@@ -0,0 +1,35 @@
+
+
+import PySimpleGUI as sg
+
+import data_access as db
+import fnmatch
+from timeit import default_timer as timer
+from datetime import timedelta
+import time
+from time import time
+
+start = timer()
+sql = "SELECT name, fullname FROM storage"
+rows = db.executeSqlStatement(sql)
+storageDict = {}
+for j in rows:
+    storageDict[j['name']] = j['fullname']
+
+end = timer()
+print('performance population dictionary storageDict= ', timedelta(seconds=end-start))
+
+print(len(storageDict))
+
+def storage_autosuggest(partialName, storage_dictionary):
+
+    print('IN storage_auto!! partial=', partialName)
+    keys = fnmatch.filter(storage_dictionary, "Box 38*")
+    print('length return dict: ', len(keys))
+
+    return keys
+
+suggestions = storage_autosuggest('Box 38', storageDict)
+print(suggestions)
+end = timer()
+print('performance running the entire module= ', timedelta(seconds=end-start))
