e13bce3491dee9e1bdc2e13959b3950b8cef6761
J. Kristoffer Legind <klegind@gmail.com>
2022-09-29 13:02:28 +0200

    cleanup and comments

--- MassDigitizer/autoSuggest_popup.py
+++ MassDigitizer/autoSuggest_popup.py
@@ -7,7 +7,7 @@ class AutoSuggest_popup():
     def __init__(self, table):
         self.tableName = table
 
-    def auto_suggest_taxonomy(self, tableName, name, taxDefItemId=None, rowLimit=200):
+    def auto_suggest(self, tableName, name, taxDefItemId=None, rowLimit=200):
         # Purpose: for helping digitizer staff rapidly input names by returning suggestions based on the three or
         #  more entered characters.
         # trigger: means how many keystrokes it takes to trigger the auto-suggest functionality
@@ -37,13 +37,9 @@ class AutoSuggest_popup():
     def autosuggest_gui(self, partialName):
         # TODO Function contract
         # Parameter partialName is the 'name' as it is being inputted, keystroke-by-keystroke
-        # The list of choices that are going to be searched
-        # In this example, the PySimpleGUI Element names are used
-        choices = self.auto_suggest_taxonomy(self.tableName, partialName)
-        print(type(choices))
 
-        print('len of choices is; ', len(choices), type(choices), '\n choices are;; ', choices)
-        # sorted([elem.__name__ for elem in sg.Element.__subclasses__()])
+        choices = self.auto_suggest(self.tableName, partialName)
+        print(type(choices))
 
         input_width = 95
         lines_to_show = 5
@@ -56,23 +52,20 @@ class AutoSuggest_popup():
                 sg.Col([[sg.Listbox(values=[], size=(input_width, lines_to_show), enable_events=True, key='-BOX-', select_mode=sg.LISTBOX_SELECT_MODE_SINGLE)]],
                        key='-BOX-CONTAINER-', pad=(0, 0), visible=True))],]
 
-        window = sg.Window('AutoComplete', layout, return_keyboard_events=True, finalize=True, modal=False,
+        window = sg.Window('Auto Complete', layout, return_keyboard_events=True, finalize=True, modal=False,
                            font=('Arial', 12), size=(810,200))
-        # The parameter "modal" is explicitly set to False. If True the auto close behavior
-        # won't work.
+        # The parameter "modal" is explicitly set to False. If True the auto close behavior won't work.
 
         list_element: sg.Listbox = window.Element('-BOX-')  # store listbox element for easier access and to get to docstrings
         prediction_list, input_text, sel_item = choices, "", 0
         window['-IN-'].update(partialName)
         window.write_event_value('-IN-', partialName)
-        # global windowAutosuggest
-        # windowAutosuggest = window
 
         while True:  # Event Loop
 
             event, values = window.read()
             operational_name = values['-IN-']
-            # print(win.close_destroys_window)
+
             if event is None:
                 print('EVENT  , NONE')
                 break
@@ -93,22 +86,22 @@ class AutoSuggest_popup():
                 # Likely to be a PySimpleGUI bug.
                 if len(values['-BOX-']) > 0:
                     boxVal = values['-BOX-']
-                    print('Selected boxval ISS - ', boxVal[0])
+                    print('Selected boxvalue is -/ '
+                          , boxVal[0])
                     window['-IN-'].update(value=boxVal[0])
                     window['-BOX-CONTAINER-'].update(visible=False)
                     return boxVal[0]
 
             elif event == '-IN-':
                 # this concerns all keystrokes except the above ones.
-                print(f'keystroking after init value...{partialName}')
                 text = values['-IN-'].lower()
-                print('current entry text:: ', text)
+
                 if text == input_text:
                     continue
                 else:
                     input_text = text
                 if len(text) < len(partialName):
-                    choices = self.auto_suggest_taxonomy(self.tableName, text)
+                    choices = self.auto_suggest(self.tableName, text)
                 prediction_list = []
                 if len(text) >= 3:
                     # condition for activating the autosuggest feature.
@@ -126,5 +119,6 @@ class AutoSuggest_popup():
                 window['-IN-'].update(value=values['-BOX-'])
                 window['-BOX-CONTAINER-'].update(visible=False)
 
+#EXE section
 r = AutoSuggest_popup('storage')
-r.autosuggest_gui('shelf 8')
+r.autosuggest_gui('box 8')
