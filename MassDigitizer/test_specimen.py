17e1368835b57ff11d1d7eba700cd48c6090da37
J. Kristoffer Legind <klegind@gmail.com>
2022-10-14 16:57:05 +0200

    added comments/documents

--- MassDigitizer/test_specimen.py
+++ MassDigitizer/test_specimen.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+  Created on October 14, 2022
+  @author: Jan K. Legind, NHMD
+  Copyright 2022 Natural History Museum of Denmark (NHMD)
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+  PURPOSE: Unit testing the specimen class.
+"""
+
+
 import pytest
 import sys
 import models
@@ -15,7 +29,7 @@ import models.specimen as collobj
 specimenObject = collobj.specimen(29)
 predefData = None
 
-def rowEater(rowObject):
+def rowObjectToList(rowObject):
     toEnumeratedList = []
     for row in rowObject:
         itemList = [col for col in row]
@@ -38,7 +52,7 @@ def test_specimen_prepTypes():
     global predefData
     print(predefData)
     sObject = specimenObject.typeStatuses
-    typeStatus = rowEater(sObject)
+    typeStatus = rowObjectToList(sObject)
     # print('--', typeStatus, '--')
     assert typeStatus[5][1][2] == 'Hapantotype'
     # Knowing for a fact that typeStatus[5][1][2] is 'Hapantotype',
