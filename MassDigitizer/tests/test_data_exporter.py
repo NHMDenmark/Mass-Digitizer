39a34ececed4442d2d5ebd02d024869cbbd6e6f1
J. Kristoffer Legind <klegind@gmail.com>
2022-10-26 08:33:06 +0200

    Create test_data_exporter.py
    initial commit

---
+++ MassDigitizer/tests/test_data_exporter.py
@@ -0,0 +1,15 @@
+import data_exporter
+import pytest
+
+
+def test_exportSpecimens():
+    export_file = data_exporter.exportSpecimens('xlsx')
+    print('¤¤¤¤¤¤¤'+export_file)
+    assert export_file == 'No specimen records to export.'
+
+def test_generteFilename():
+    export_path = data_exporter.generateFilename('my_specimens', 'xlsx',
+                                                 r"C:\Users\bxq762\Documents\workspace\Mass digitizer\DaSSCo\MassDigitizer\output")
+    print('%%%%%%%'+export_path)
+    assert export_path
+
