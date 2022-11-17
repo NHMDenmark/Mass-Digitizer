8e0a5398aa3c68f3e2f69c7f77b090ea59d94b74
J. Kristoffer Legind <klegind@gmail.com>
2022-11-17 09:37:53 +0100

    Update test_collection.py
    Comment added

--- MassDigitizer/tests/test_collection.py
+++ MassDigitizer/tests/test_collection.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+  Created on September 28, 2022
+  @author: Fedor Alexander Steeman, NHMD
+  Copyright 2022 Natural History Museum of Denmark (NHMD)
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+  PURPOSE: Test collection module
+"""
+"""
+PLEASE RUN THIS TEST USING -s :
+ python -m pytest -s .\tests\test_collection.py
+"""
+
 from models import collection
 import specify_interface
 import global_settings
