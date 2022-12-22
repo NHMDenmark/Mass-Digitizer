7ebf3834a85a87851ae0c6d799e85cb1618a191c
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2022-12-22 16:47:37 +0100

    regenerated taxon spine sql & emptied db #185

--- MassDigitizer/sql/ExtractTaxonNames.sql
+++ MassDigitizer/sql/ExtractTaxonNames.sql
@@ -1,7 +1,8 @@
- SELECT 	
+  SELECT 	
 	-- t.TaxonID taxonid, -- t.Name name, t.FullName fullname, t.RankID rankid, t.TaxonTreeDefID taxontreedefid, p1.FullName parentfullname, 
-	CONCAT('INSERT INTO taxonname ("taxonid","name","fullname","rankid","taxontreedefid","parentfullname") VALUES (',
-			 t.TaxonID, ',"', t.Name, '","', t.FullName, '",', t.RankID, ',', t.TaxonTreeDefID, ',"', p1.FullName,'");') 
+	CONCAT(
+	-- 'INSERT INTO taxonname ("taxonid","name","fullname","rankid","taxontreedefid","parentfullname") VALUES (',
+	',(', t.TaxonID, ',"', t.Name, '","', t.FullName, '",', t.RankID, ',', t.TaxonTreeDefID, ',"', p1.FullName,'")') 
 	sqlstatement 
  FROM taxon t 
 	JOIN taxon p1 ON p1.TaxonID = t.ParentID
@@ -9,4 +10,9 @@
 		t.taxontreedefid = 13
 		AND t.FullName NOT LIKE '%*%' 
 		AND t.fullname NOT LIKE '%.%' 
-LIMIT 300000 -- OFFSET 300000
+		AND t.RankID <= 180 -- Highertaxa (including Genus)
+		-- AND t.RankID = 240 -- Species 
+		-- AND t.RankID = 230 -- Subspecies 
+	ORDER BY t.RankID -- For higher taxa
+	-- ORDER BY t.fullName -- For species and below
+LIMIT 250000 -- OFFSET 250000
