c29eced83ea9a4075bcd6e4ae703d917fbfa3ac3
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2022-12-22 16:47:37 +0100

    regenerated taxon spine sql & emptied db #185

---
+++ MassDigitizer/sql/editions/Insert-Template.sql
@@ -0,0 +1,9 @@
+PRAGMA journal_mode = OFF;
+PRAGMA synchronous = 0;
+PRAGMA cache_size = 1000000;
+PRAGMA locking_mode = EXCLUSIVE;
+PRAGMA temp_store = MEMORY;
+BEGIN TRANSACTION;
+INSERT INTO taxonname ("spid","name","fullname","rankid","taxontreedefid","parentfullname") VALUES 
+
+;
