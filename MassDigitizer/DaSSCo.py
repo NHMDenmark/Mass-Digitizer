fc46b6f603faebdc233ad5eecb7a043a26a739bb
J. Kristoffer Legind <klegind@gmail.com>
2023-01-12 13:25:00 +0100

    added logging and a bit of cleanup

--- MassDigitizer/DaSSCo.py
+++ MassDigitizer/DaSSCo.py
@@ -1,16 +1,35 @@
 import sys
 from pathlib import Path
+import logging
+import pathlib
+import time
+import os
 
 # Below line is needed for accessing internal dependencies 
 sys.path.append(str(Path(__file__).parent.parent.joinpath('')))
 
 import home_screen
-import data_access
-import specify_interface
+import util
+# import specify_interface
+
+sTime = time.strftime('{%Y-%m-%d_%H,%M,%S}').replace("{", "").replace("}", "")
+pathString = util.getLogsPath()
+filePath = os.path.expanduser(pathString)
+sys.path.append(str(pathlib.Path(__file__).parent.parent.joinpath(filePath)))
+logName = f"DaSSCo_py{sTime}.log"
+logFilePath = str(Path(filePath).joinpath(f'{logName}'))
+
+
+logging.basicConfig(filename=logFilePath, encoding='utf-8', level=logging.DEBUG, force=True)
+
+logging.debug('DaSSCo initial app log write! --- !!!!!!!!!!')
+# l = util.buildLogger('DaSSCo_py')
 
 def main() -> None:
-    print()
-    print("* Mass Digitizer for DaSSCo *")
+    try:
+        logging.debug("* Mass Digitizer for DaSSCo *")
+    except Exception as e:
+        logging.debug(str(e))
 
     home = home_screen.HomeScreen()
 
