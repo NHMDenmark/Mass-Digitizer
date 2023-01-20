136e0cb364cf28b09438c82614aa6961b38f88e2
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-01-20 14:39:56 +0100

    renamed files in preparation for #212

---
+++ MassDigitizer/sql/ExtractPrepTypes.sql
@@ -0,0 +1,7 @@
+  -- To be run on Specify database for each collection 
+  	SELECT    
+	CONCAT (',(', pt.PrepTypeID, ',"', pt.Name, '", (SELECT id FROM collection WHERE spid = ', pt.CollectionID, '))') sqlstatement 
+ FROM preptype pt 
+ 		WHERE pt.CollectionID = 0 
+		-- Adjust collection id : 
+		--    NHMD Tracheophyta - 688130
