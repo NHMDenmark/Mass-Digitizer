4e2faa6388ebeed90c7fa229d9d64c664a8af249
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-01-20 15:19:42 +0100

    smarter taxonname extraction #212

--- MassDigitizer/editions/botany-tracheo/NHMD-tracheophyta-prepare-db.bat
+++ MassDigitizer/editions/botany-tracheo/NHMD-tracheophyta-prepare-db.bat
@@ -10,37 +10,42 @@ echo Copied db base file to temporary folder
 cd ..\..\temp\
 echo switched to temporary folder 
 
-copy ..\sql\editions\botany\tracheophyta\Tracheophyta-Highertaxa.sql Tracheophyta-Highertaxa.sql
+copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-PrepTypes.sql Tracheophyta-PrepTypes.sql
+sqlite3 db.sqlite3 ".read Tracheophyta-PrepTypes.sql"
+del /f Tracheophyta-PrepTypes.sql
+echo Added preparation types...
+
+copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Highertaxa.sql Tracheophyta-Highertaxa.sql
 sqlite3 db.sqlite3 ".read Tracheophyta-Highertaxa.sql"
 del /f Tracheophyta-Highertaxa.sql
 echo Added higher taxa...
 
-copy ..\sql\editions\botany\tracheophyta\Tracheophyta-Species-Batch1.sql Tracheophyta-Species-Batch1.sql
+copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Species-Batch1.sql Tracheophyta-Species-Batch1.sql
 sqlite3 db.sqlite3 ".read Tracheophyta-Species-Batch1.sql"
 del /f Tracheophyta-Species-Batch1.sql
 echo Added species batch 1...
 
-copy ..\sql\editions\botany\tracheophyta\Tracheophyta-Species-Batch2.sql Tracheophyta-Species-Batch2.sql
+copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Species-Batch2.sql Tracheophyta-Species-Batch2.sql
 sqlite3 db.sqlite3 ".read Tracheophyta-Species-Batch2.sql"
 del /f Tracheophyta-Species-Batch2.sql
 echo Added species batch 2...
 
-copy ..\sql\editions\botany\tracheophyta\Tracheophyta-Species-Batch3.sql Tracheophyta-Species-Batch3.sql
+copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Species-Batch3.sql Tracheophyta-Species-Batch3.sql
 sqlite3 db.sqlite3 ".read Tracheophyta-Species-Batch3.sql"
 del /f Tracheophyta-Species-Batch3.sql
 echo Added species batch 3...
 
-copy ..\sql\editions\botany\tracheophyta\Tracheophyta-Species-Batch4.sql Tracheophyta-Species-Batch4.sql
+copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Species-Batch4.sql Tracheophyta-Species-Batch4.sql
 sqlite3 db.sqlite3 ".read Tracheophyta-Species-Batch4.sql"
 del /f Tracheophyta-Species-Batch4.sql
 echo Added species batch 4...
 
-copy ..\sql\editions\botany\tracheophyta\Tracheophyta-Subspecies.sql Tracheophyta-Subspecies.sql
+copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Subspecies.sql Tracheophyta-Subspecies.sql
 sqlite3 db.sqlite3 ".read Tracheophyta-Subspecies.sql"
 del /f Tracheophyta-Subspecies.sql
 echo Added subspecies...
 
-copy ..\sql\editions\botany\tracheophyta\Tracheophyta-VarForma.sql Tracheophyta-VarForma.sql
+copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-VarForma.sql Tracheophyta-VarForma.sql
 sqlite3 db.sqlite3 ".read Tracheophyta-VarForma.sql"
 del /f Tracheophyta-VarForma.sql
 echo Added infraspecific taxa other than subspecies...
