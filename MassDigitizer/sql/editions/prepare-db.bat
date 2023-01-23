f9b69b108dc64201649ba0ee958d13af83f77780
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-01-23 18:56:35 +0100

    universal batch script #212

--- MassDigitizer/sql/editions/prepare-db.bat
+++ MassDigitizer/sql/editions/prepare-db.bat
@@ -1,69 +1,39 @@
 @echo off 
 Rem Remember to install sqlite command tools first -> https://sqlite.org/download.html 
 Rem Then add to PATH environment variables 
+echo Preparing db edition
+echo %1
 
-echo Preparing db edition... 
+IF [%1] == [] GOTO :NoParameter
+
+copy ..\sql\editions\%1\test.txt test.txt
+if %errorlevel% NEQ 0 GOTO :InvalidPath
 
 copy /y ..\..\db\db.sqlite3 ..\..\temp\db.sqlite3
 echo Copied db base file to temporary folder 
 
 cd ..\..\temp\
-echo switched to temporary folder 
+echo switched to temporary folder
 
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-PrepTypes.sql PrepTypes.sql
-sqlite3 db.sqlite3 ".read PrepTypes.sql"
-del /f PrepTypes.sql
-echo Added preparation types...
+set list=PrepTypes TypeStatuses GeoRegions Storage Highertaxa Species-Batch1 Species-Batch2 Species-Batch3 Species-Batch4 Subspecies VarForma
+(for %%t in (%list%) do ( 
+    echo Reading %%t
+    copy ..\sql\editions\%1\%%t.sql %%t.sql
+    sqlite3 db.sqlite3 ".read %%t.sql"
+    echo Added %%t
+    del /f %%t.sql
+    echo *********************
+))
 
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-TypeStatuses.sql TypeStatuses.sql
-sqlite3 db.sqlite3 ".read TypeStatuses.sql"
-del /f TypeStatuses.sql
-echo Added type statuses...
+cd ..\sql\editions
+echo Exiting... 
 
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-GeoRegions.sql GeoRegions.sql
-sqlite3 db.sqlite3 ".read GeoRegions.sql"
-del /f GeoRegions.sql
-echo Added broad geographic regions...
+exit /b 0 
 
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Storage.sql Storage.sql
-sqlite3 db.sqlite3 ".read Storage.sql"
-del /f GeoRegions.sql
-echo Added storage locations...
+:NoParameter
+echo Please add parameter for edition path
+exit /b 1
 
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Highertaxa.sql Highertaxa.sql
-sqlite3 db.sqlite3 ".read Highertaxa.sql"
-del /f Highertaxa.sql
-echo Added higher taxa...
-
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Species-Batch1.sql Species-Batch1.sql
-sqlite3 db.sqlite3 ".read Species-Batch1.sql"
-del /f Species-Batch1.sql
-echo Added species batch 1...
-
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Species-Batch2.sql Species-Batch2.sql
-sqlite3 db.sqlite3 ".read Species-Batch2.sql"
-del /f Species-Batch2.sql
-echo Added species batch 2...
-
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Species-Batch3.sql Species-Batch3.sql
-sqlite3 db.sqlite3 ".read Species-Batch3.sql"
-del /f Species-Batch3.sql
-echo Added species batch 3...
-
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Species-Batch4.sql Species-Batch4.sql
-sqlite3 db.sqlite3 ".read Species-Batch4.sql"
-del /f Species-Batch4.sql
-echo Added species batch 4...
-
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-Subspecies.sql Subspecies.sql
-sqlite3 db.sqlite3 ".read Subspecies.sql"
-del /f Subspecies.sql
-echo Added subspecies...
-
-copy ..\sql\editions\NHMD\tracheophyta\NHMD-Tracheophyta-VarForma.sql VarForma.sql
-sqlite3 db.sqlite3 ".read VarForma.sql"
-del /f VarForma.sql
-echo Added infraspecific taxa other than subspecies...
-
-xcopy ..\DaSSCo.dist\* DaSSCo.dist\*
-copy /y ..\temp\db.sqlite3 temp\db.sqlite3
+:InvalidPath
+echo Please add a valid parameter for edition path
+exit /b 1
