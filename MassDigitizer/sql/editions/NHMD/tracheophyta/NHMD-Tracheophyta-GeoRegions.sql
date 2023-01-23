6a5642b003ac5de630a176c4d32987529eb40b20
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-01-23 15:46:59 +0100

    ditches bgr source as separate table #212

--- MassDigitizer/sql/editions/NHMD/tracheophyta/NHMD-Tracheophyta-GeoRegions.sql
+++ MassDigitizer/sql/editions/NHMD/tracheophyta/NHMD-Tracheophyta-GeoRegions.sql
@@ -1,12 +1,12 @@
-INSERT INTO "main"."georegion" ("id", "name", "sourceid", "collectionid") VALUES 
-('1', 'Dania', '11', '1'),
-('2', 'Fennoscandia', '1', '29'),
-('3', 'Grønland', '1', '29'),
-('4', 'Europe', '1', '29'),
-('5', 'Africa', '1', '29'),
-('6', 'America centralis et australis et Antarctica', '1', '29'),
-('7', 'America septentrionalis', '1', '29'),
-('8', 'Asia', '1', '29'),
-('9', 'Australia et Oceania', '1', '29'),
-('10', 'Færøerne', '1', '29'),
-('11', 'Island', '1', '29');
+INSERT INTO "main"."georegion" ("name", "source", "collectionid") VALUES 
+('Dania',  'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('Fennoscandia', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('Grønland', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('Europe', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('Africa', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('America centralis et australis et Antarctica', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('America septentrionalis', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('Asia', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('Australia et Oceania', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('Færøerne', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130)),
+('Iceland', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130));
