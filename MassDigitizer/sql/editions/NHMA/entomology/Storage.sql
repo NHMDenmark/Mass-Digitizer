f2ca8c9b4ca175ce337933ec187f48d4c1f808d8
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-01-25 13:43:04 +0100

    Expanded editions' predef data

--- MassDigitizer/sql/editions/NHMA/entomology/Storage.sql
+++ MassDigitizer/sql/editions/NHMA/entomology/Storage.sql
@@ -1,1 +1,5 @@
-.
+INSERT INTO storage (name, fullname, collectionid) VALUES 
+("Lepidoptera DK (Pinned)","Museum Storage (Universitetsparken) | Entomology Collections | Lepidoptera DK (Pinned)",(SELECT id FROM collection WHERE spid = 32769 AND institutionid = 2)),
+("Non-Lepidoptera DK (Pinned)","Museum Storage (Universitetsparken) | Entomology Collections | Non-Lepidoptera DK (Pinned)",(SELECT id FROM collection WHERE spid = 32769 AND institutionid = 2)),
+("Transit Entomology (Pinned)","Museum Storage (Universitetsparken) | Entomology Collections | Transit Entomology (Pinned)",(SELECT id FROM collection WHERE spid = 32769 AND institutionid = 2))
+;
