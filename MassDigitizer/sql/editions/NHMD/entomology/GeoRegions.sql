10296b9172486cda8e65bb39d67135cebc25d7da
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-01-30 15:14:23 +0100

    Adjustments to BGeoRegions #

--- MassDigitizer/sql/editions/NHMD/entomology/GeoRegions.sql
+++ MassDigitizer/sql/editions/NHMD/entomology/GeoRegions.sql
@@ -1,7 +1,9 @@
 INSERT INTO "main"."georegion" ("name", "source", "collectionid") VALUES 
- ('Nearctic',     'Major biogeographical region (Hansen 1998, World Catalogue of Insects)', (SELECT id FROM collection WHERE spid = 163841 AND institutionid = 1))
+ ('None',         'none', (SELECT id FROM collection WHERE spid = 163841 AND institutionid = 1))
+,('Nearctic',     'Major biogeographical region (Hansen 1998, World Catalogue of Insects)', (SELECT id FROM collection WHERE spid = 163841 AND institutionid = 1))
 ,('Neotropical',  'Major biogeographical region (Hansen 1998, World Catalogue of Insects)', (SELECT id FROM collection WHERE spid = 163841 AND institutionid = 1))
 ,('Palearctic',   'Major biogeographical region (Hansen 1998, World Catalogue of Insects)', (SELECT id FROM collection WHERE spid = 163841 AND institutionid = 1))
 ,('Afrotropical', 'Major biogeographical region (Hansen 1998, World Catalogue of Insects)', (SELECT id FROM collection WHERE spid = 163841 AND institutionid = 1))
 ,('Oriental',     'Major biogeographical region (Hansen 1998, World Catalogue of Insects)', (SELECT id FROM collection WHERE spid = 163841 AND institutionid = 1))
 ,('Australian',   'Major biogeographical region (Hansen 1998, World Catalogue of Insects)', (SELECT id FROM collection WHERE spid = 163841 AND institutionid = 1))
+,('Danian',       'none', (SELECT id FROM collection WHERE spid = 163841 AND institutionid = 1))
