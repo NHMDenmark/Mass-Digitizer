245efd083dcafa2a55103e1cb6eb5e3b8e8926c2
J. Kristoffer Legind <klegind@gmail.com>
2023-01-31 14:37:40 +0100

    forced by 'No new-line at EoF

--- MassDigitizer/sql/ExtractStorageLocations.sql
+++
@@ -1,8 +0,0 @@
-  -- To be run on Specify database for each collection 
-  	SELECT    
-	CONCAT (',(', st.StorageID, ',"', st.Name, , ',"', st.Name, 
-            '", (SELECT id FROM collection WHERE spid = ', pt.CollectionID, 
-		    ' AND institutionid = 1))') -- Adjust institutionid : NHMD - 1, NHMA - 2  
-	sqlstatement 
- FROM storage st 
- 		 
