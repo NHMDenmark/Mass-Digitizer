29f454d9017c3fb16cdcc91c0f1455baeb1476dd
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-02-08 12:16:21 +0100

    Fixed wrong BGR names #251

--- MassDigitizer/sql/editions/NHMD/tracheophyta/GeoRegions.sql
+++ MassDigitizer/sql/editions/NHMD/tracheophyta/GeoRegions.sql
@@ -1,5 +1,5 @@
 INSERT INTO "main"."georegion" ("name", "source", "collectionid") VALUES 
-('Denmark',  'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1)),
+('Dania',  'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1)),
 ('None', 'none', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1)),
 ('Fennoscandia', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1)),
 ('Grønland', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1)),
@@ -10,6 +10,6 @@ INSERT INTO "main"."georegion" ("name", "source", "collectionid") VALUES
 ('Asia', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1)),
 ('Australia et Oceania', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1)),
 ('Færøerne', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1)),
-('Iceland', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1));
+('Island', 'Major biogeographical region (NHMD)', (SELECT id FROM collection WHERE spid = 688130 AND institutionid = 1));
 
 
