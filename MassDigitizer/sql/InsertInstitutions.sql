905ff15b312cb4e572c9b116fe7fd72cb4f012df
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-02-08 11:58:06 +0100

    Restored deleted SQL files

---
+++ MassDigitizer/sql/InsertInstitutions.sql
@@ -0,0 +1,10 @@
+INSERT INTO "main"."institution" ("id", "code", "name", "url") VALUES 
+('1', 'NHMD', 'Natural History Museum of Denmark', 'https://specify-snm.science.ku.dk/'),
+('2', 'NHMA', 'Natural History Museum Aarhus', 'https://specify-nhma.science.ku.dk/'),
+('3', 'AU', 'Aarhus University Herbarium', 'https://specify-test.science.ku.dk/'),
+('4', 'MMG', 'Museum Mors & Fur Museum', ' https://specify-muserum.science.ku.dk/'),
+('5', 'FIMUS', 'Fiskeri- og Søfartmuseet', 'https://specify-fimus.science.ku.dk/'),
+('6', 'MSJN', 'Museum Sønderjylland (Gram)', 'https://specify-msjn.science.ku.dk/'),
+('7', 'S', 'Naturama', 'https://specify-naturama.science.ku.dk/'),
+('8', 'OESM', 'Østsjællands Museum (Faxe)', 'https://specify-oesm.science.ku.dk/'),
+('99', 'TEST', 'Test Institution', 'https://specify-test.science.ku.dk/');
