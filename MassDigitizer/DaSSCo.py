78e95f2826e72cc40b23e1bd27755550d25619d3
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-02-09 12:22:55 +0100

    Centralized logging & Created single edition #260

--- MassDigitizer/DaSSCo.py
+++ MassDigitizer/DaSSCo.py
@@ -12,24 +12,13 @@ import home_screen
 import util
 # import specify_interface
 
-sTime = time.strftime('{%Y-%m-%d_%H,%M,%S}').replace("{", "").replace("}", "")
-pathString = util.getLogsPath()
-filePath = os.path.expanduser(pathString)
-sys.path.append(str(pathlib.Path(__file__).parent.parent.joinpath(filePath)))
-logName = f"DaSSCo_py{sTime}.log"
-logFilePath = str(Path(filePath).joinpath(f'{logName}'))
-
-
-logging.basicConfig(filename=logFilePath, encoding='utf-8', level=logging.DEBUG, force=True)
-
-logging.debug('DaSSCo initial app log write! --- !!!!!!!!!!')
-# l = util.buildLogger('DaSSCo_py')
+util.buildLogger()
 
 def main() -> None:
     try:
-        logging.debug("* Mass Digitizer for DaSSCo *")
+        util.logging.debug("* Mass Digitizer for DaSSCo *")
     except Exception as e:
-        logging.debug(str(e))
+        util.logging.debug(str(e))
 
     home = home_screen.HomeScreen()
 
