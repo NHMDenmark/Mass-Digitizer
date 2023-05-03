9fdf33184d8d8775f6030c8efb9c5f55414fcd61
Jan Krzysztof Legind <bxq762@ku.dk>
2023-05-03 16:23:42 +0200

    Arhus taxonomy

--- MassDigitizer/sql/editions/NHMA/entomology/VarForma.sql
+++ MassDigitizer/sql/editions/NHMA/entomology/VarForma.sql
@@ -1,1 +1,3 @@
--- No further records
+INSERT INTO taxonname (spid,name,fullname,rankid,taxonRank, treedefid,parentfullname) VALUES
+(8285,"pascuorum","Bombus pascuorum pascuorum",	240,"variety", 2,"Bombus pascuorum")
+,(8287,"mniorum","Bombus pascuorum mniorum",	240,"variety", 2,"Bombus pascuorum");
