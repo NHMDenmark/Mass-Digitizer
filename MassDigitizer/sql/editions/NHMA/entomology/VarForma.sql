33804a651bfcbf853f92d1e7824bdc2d751254d8
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-05-12 10:32:31 +0200

    Added missing varietas for NHMA ENT

--- MassDigitizer/sql/editions/NHMA/entomology/VarForma.sql
+++ MassDigitizer/sql/editions/NHMA/entomology/VarForma.sql
@@ -1,3 +1,4 @@
 INSERT INTO taxonname (spid,name,fullname,rankid,taxonRank, treedefid,parentfullname) VALUES
-(8285,"pascuorum","Bombus pascuorum pascuorum",	240,"variety", 2,"Bombus pascuorum")
-,(8287,"mniorum","Bombus pascuorum mniorum",	240,"variety", 2,"Bombus pascuorum");
+(8285,"pascuorum","Bombus pascuorum pascuorum",	240,"variety", 2,"Bombus pascuorum"),
+(8286,"tricuspis","Bombus pascuorum tricuspis",	240,"Variety", 2,"Bombus pascuorum"),
+(8287,"mniorum","Bombus pascuorum mniorum",	240,"variety", 2,"Bombus pascuorum");
