d7c801e10b4b7395227cf8103188c3695dacc8b0
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-05-12 16:23:12 +0200

    tweaks, fixes & stubs to merge duplicate function

--- MassDigitizer/sql/GetTaxonDuplicates.sql
+++ MassDigitizer/sql/GetTaxonDuplicates.sql
@@ -1,4 +1,4 @@
-SELECT t1.TaxonID -- , t2.TaxonID, t1.FullName, t1.Author, t2.FullName, t2.Author  
+SELECT DISTINCT(t1.TaxonID) -- , t2.TaxonID, t1.FullName, t1.Author, t2.FullName, t2.Author  
 FROM taxon t1 
 	LEFT JOIN taxon t2 ON t1.FullName = t2.FullName AND t1.TaxonID <> t2.TaxonID 
 	WHERE t1.TaxonTreeDefID = 13 AND t2.TaxonTreeDefID = 13 -- Restrict to NHMD Botany 
