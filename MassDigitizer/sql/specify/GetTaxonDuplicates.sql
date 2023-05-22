647dc7bc7417e00c007f088fd1595aa53896ac01
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-05-22 19:01:28 +0200

    Regenerated taxon spine scripts #313

---
+++ MassDigitizer/sql/specify/GetTaxonDuplicates.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT(t1.TaxonID) -- , t2.TaxonID, t1.FullName, t1.Author, t2.FullName, t2.Author  
+FROM taxon t1 
+	LEFT JOIN taxon t2 ON t1.FullName = t2.FullName AND t1.TaxonID <> t2.TaxonID 
+	WHERE t1.TaxonTreeDefID = 13 AND t2.TaxonTreeDefID = 13 -- Restrict to NHMD Botany 
+	  AND t1.RankID >= 180 AND t2.RankID >= 180 -- Restrict to genus and below 
+	  
+	  ORDER BY t1.TaxonId ;
