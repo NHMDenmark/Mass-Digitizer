734a880c424cc9b21170a26e3fd97292fc8ab0a0
Jan Krzysztof Legind <bxq762@ku.dk>
2023-08-29 16:44:35 +0200

    Implemented shared module for global barcode validation.

--- MassDigitizer/global_settings.py
+++ MassDigitizer/global_settings.py
@@ -9,8 +9,25 @@
   http://www.apache.org/licenses/LICENSE-2.0
   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-  PURPOSE: Assemblage of global settings used across the application. 
+  PURPOSE: Assemblage of global settings used across the application.
+
+  PURPOSE of sharedValidation: Make available a globally shared JSON struct to hold variables like 'catalog number length' :
+  {"catalogNumberLength_NHMDvascularPlants": 8, "catalogNumberLength_NHMDentomology": 9} which can be used in multiple places.
 """
+
+from shared import Dossier, HOME
+from pathlib import Path
+
+import os
+# Shared JSON struct
+
+validationData ={"catalogNumberLength_NHMDvascularPlants": 8, "catalogNumberLength_NHMDentomology": 9, "catalogNumberLength_NHMAentomology": 8}
+path = Path(HOME, 'shared_vars')
+validationGlobal = Dossier(path)
+validationGlobal.set('shared_vars', validationData)
+
+shared_out = validationGlobal.get("shared_vars")
+print('shared_out', shared_out)
 
 # Specify 
 baseURL = ''
@@ -31,6 +48,7 @@ userName = ''
 spUserId = -1
 # agentFullName = ' '.join([spFirstName, spMiddleInitial, spLastName])
 csrfToken = ''
+lengthCatalogNumber = 0
 
 def clearSession():
   institutionId = 0
