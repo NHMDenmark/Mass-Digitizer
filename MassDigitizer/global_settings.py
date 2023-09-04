9c2282e55031995bdfcb69a6d229535eb34e8e0c
Jan Krzysztof Legind <bxq762@ku.dk>
2023-09-04 09:20:00 +0200

    Added global 'lengthCatalogNumber' for barcode validation.

--- MassDigitizer/global_settings.py
+++ MassDigitizer/global_settings.py
@@ -9,25 +9,8 @@
   http://www.apache.org/licenses/LICENSE-2.0
   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-  PURPOSE: Assemblage of global settings used across the application.
-
-  PURPOSE of sharedValidation: Make available a globally shared JSON struct to hold variables like 'catalog number length' :
-  {"catalogNumberLength_NHMDvascularPlants": 8, "catalogNumberLength_NHMDentomology": 9} which can be used in multiple places.
+  PURPOSE: Assemblage of global settings used across the application. 
 """
-
-from shared import Dossier, HOME
-from pathlib import Path
-
-import os
-# Shared JSON struct
-
-validationData ={"catalogNumberLength_NHMDvascularPlants": 8, "catalogNumberLength_NHMDentomology": 9, "catalogNumberLength_NHMAentomology": 8}
-path = Path(HOME, 'shared_vars')
-validationGlobal = Dossier(path)
-validationGlobal.set('shared_vars', validationData)
-
-shared_out = validationGlobal.get("shared_vars")
-print('shared_out', shared_out)
 
 # Specify 
 baseURL = ''
