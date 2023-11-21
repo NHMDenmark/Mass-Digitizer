5b8994ffcffdf27d20aec4834a86cba644caa5bc
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-11-21 11:14:58 +0100

    Quick fix for NHMA Storage predefined data
    The storage items defined for NHMA Entomology did not included rankname yet, and neither was the institution included in the fullname. This could pose issues during postprocessing, since the storagefullname string is split on pipe (|)  symbols and each element assigned to the columns institution, collection, and room, respectively.

--- MassDigitizer/sql/editions/NHMA/entomology/Storage.sql
+++ MassDigitizer/sql/editions/NHMA/entomology/Storage.sql
@@ -1,5 +1,5 @@
-INSERT INTO storage (name, fullname, collectionid) VALUES 
-("Lepidoptera DK (Pinned)","Museum Storage (Universitetsparken) | Entomology Collections | Lepidoptera DK (Pinned)",(SELECT id FROM collection WHERE spid = 32769 AND institutionid = 2)),
-("Non-Lepidoptera DK (Pinned)","Museum Storage (Universitetsparken) | Entomology Collections | Non-Lepidoptera DK (Pinned)",(SELECT id FROM collection WHERE spid = 32769 AND institutionid = 2)),
-("Transit Entomology (Pinned)","Museum Storage (Universitetsparken) | Entomology Collections | Transit Entomology (Pinned)",(SELECT id FROM collection WHERE spid = 32769 AND institutionid = 2))
+INSERT INTO storage (name, fullname, rankname, collectionid) VALUES 
+("Lepidoptera DK (Pinned)","Natural History Museum Aarhus | Museum Storage (Universitetsparken) | Entomology Collections | Lepidoptera DK (Pinned)", "Collection", (SELECT id FROM collection WHERE spid = 32769 AND institutionid = 2)),
+("Non-Lepidoptera DK (Pinned)","Natural History Museum Aarhus | Museum Storage (Universitetsparken) | Entomology Collections | Non-Lepidoptera DK (Pinned)", "Collection", (SELECT id FROM collection WHERE spid = 32769 AND institutionid = 2)),
+("Transit Entomology (Pinned)","Natural History Museum Aarhus | Museum Storage (Universitetsparken) | Entomology Collections | Transit Entomology (Pinned)", "Collection", (SELECT id FROM collection WHERE spid = 32769 AND institutionid = 2))
 ;
