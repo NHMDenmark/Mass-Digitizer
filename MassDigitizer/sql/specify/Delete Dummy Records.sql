37c418ae17edd004fed4885ecbe3e2019206962c
Fedor Alexander Steeman <fedor.steeman@gmail.com>
2023-12-18 13:45:39 +0100

    Inconsequential changes to SQL scripts

--- MassDigitizer/sql/specify/Delete Dummy Records.sql
+++ MassDigitizer/sql/specify/Delete Dummy Records.sql
@@ -1,4 +1,11 @@
  DELETE FROM collectionobject  
--- SELECT * FROM collectionobject  
-	WHERE ProjectNumber = 'Digitisation' -- 'DaSSCo' 
-	  AND catalogNumber BETWEEN 00000000 AND 00000000 
+	WHERE ProjectNumber = 'Reserved for DaSSCo' -- 'DaSSCo' 
+	  AND -- catalognumber = '001650877'
+	  		-- (catalogNumber BETWEEN 001700807      AND 1700806 ) OR
+	      -- (catalogNumber BETWEEN 001650854 AND 001650876) OR
+	      -- (catalogNumber BETWEEN 1650279 AND 1650503) OR
+	      (catalogNumber BETWEEN 001700807      AND 001700948 )
+	      
+
+
+
